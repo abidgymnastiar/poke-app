9d04e0055f488b560bd1578051b19748f92bec93
abidgymnastiar <abidgymnastiar@gmail.com>
2025-02-28 16:42:15 +0700

    fix: deploy vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HeroPokemon from "./pages/HeroPokemon";
 import DetailPokemon from "./pages/DetailPokemon";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HeroPokemon />} />
-          <Route path="/detail/:id" element={<DetailPokemon />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroPokemon />} />
+        <Route path="/detail/:id" element={<DetailPokemon />} />
+      </Routes>
+    </Router>
   );
 }
 
